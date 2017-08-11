3d9bddfe3059b154aceb478eaa9c1c9e66acf652
aloy <loyad01@gmail.com>
2017-08-11 09:25:52 -0500

    added doc for @distribution and @dparams in qq_line and qq_band

--- R/stat_qq_line.R
+++ R/stat_qq_line.R
@@ -7,6 +7,16 @@
 #' @inheritParams ggplot2::layer
 #' @inheritParams ggplot2::geom_path
 #'
+#' @param distribution Character. Theoretical probability distribution function
+#'   to use. Do not provide the full distribution function name (e.g.,
+#'   \code{"dnorm"}). Instead, just provide its shortened name (e.g.,
+#'   \code{"norm"}). If you wish to provide a custom distribution, you may do so
+#'   by first creating the density, quantile, and random functions following the
+#'   standard nomenclature from the \code{stats} package (i.e., for
+#'   \code{"custom"}, create the \code{"dcustom"}, \code{"qcustom"}, and
+#'   \code{"rcustom"} functions).
+#' @param dparams List of additional parameters passed on to the previously
+#'   chosen \code{distribution} function.
 #' @param detrend Logical. Should the plot objects be detrended? If \code{TRUE},
 #'   the objects will be detrended according to the reference Q-Q line. This
 #'   procedure was described by Thode (2002), and may help reducing visual bias
