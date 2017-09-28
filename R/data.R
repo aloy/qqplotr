16aae6065792ec396e78a04b045e4f496117f7b2
aloy <loyad01@gmail.com>
2017-09-28 11:09:06 -0500

    adding longjump data + help file

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Men's Olympic Long Jump Qualifiers 2012
+#'
+#' @name longjump
+#' @docType data
+#' @keywords datasets
+#' @usage data(longjump)
+#' @format A data frame with 42 observations on the following 4 variables:
+#' \describe{
+#' \item{rank}{Athlete's rank at the qualifying event}
+#' \item{name}{Athlete's name}
+#' \item{country}{Athlete's country of origin}
+#' \item{distance}{Result in meters}
+#' }
+#' @source \url{https://www.olympic.org/london-2012/athletics/long-jump-men}
+"longjump"
+
