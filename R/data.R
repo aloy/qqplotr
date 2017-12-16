ec8bed9b849bf43d78eca7f277e59f9637c4ccf4
Heike Hofmann <hofmann@iastate.edu>
2017-12-16 13:32:07 -0600

    added subset of the brfss data to the package

--- R/data.R
+++ R/data.R
@@ -14,3 +14,18 @@
 #' @source \url{https://www.olympic.org/london-2012/athletics/long-jump-men}
 "longjump"
 
+
+#' 2012 BRFSS sample for the state of Iowa
+#'
+#' @name iowa
+#' @docType data
+#' @keywords datasets
+#' @usage data(iowa)
+#' @format A data frame with 7166 observations on 3 variables:
+#' \describe{
+#' \item{SEX}{Gender}
+#' \item{WTKG3}{Weight in kg}
+#' \item{HTIN4}{Height in inch}
+#' }
+#' @source \url{https://www.cdc.gov/brfss/annual_data/annual_2012.html}
+"iowa"
